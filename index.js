7c84a8d8466454f621c5ec6889a87612586501b8
SnakeLuck360GT <56480665+SnakeLuck360GT@users.noreply.github.com>
2021-08-29 16:06:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const bot = new Discord.Client({ disableMentions: 'everyone' });
 
 
 venom
-  .create()
-  .then((client) => start(client))
+  .create(
+    'DiscordBot', 
+    {
+      folderNameToken: 'tokens', //folder name when saving tokens
+      mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+      headless: true, // Headless chrome
+      devtools: false, // Open devtools by default
+      useChrome: true, // If false will use Chromium instance
+      debug: false, // Opens a debug session
+      logQR: true, // Logs QR automatically in terminal
+      browserWS: '', // If u want to use browserWSEndpoint
+      browserArgs: [''], //Original parameters  ---Parameters to be added into the chrome browser instance
+      puppeteerOptions: {args:['--no-sandbox']}, // Will be passed to puppeteer.launch
+      disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+      disableWelcome: true, // Will disable the welcoming message which appears in the beginning
+      updatesLog: true, // Logs info updates automatically in terminal
+      autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
+      createPathFileToken: false, //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
+    }
+  )
+  .then((client) => {
+    start(client);
+  })
   .catch((erro) => {
     console.log(erro);
   });
